7d32337bd6079179faaeb0a7ab78dce41bb7bca8
esc5221 <esc5221@gmail.com>
2022-05-30 23:51:18 +0900

    add : flask_cors, GET /products

--- flask/www/app.py
+++ flask/www/app.py
@@ -1,13 +1,35 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response
+from flask import Flask, render_template, request, redirect, url_for, make_response, jsonify, abort
+from flask_cors import CORS
+
+import sys
 
 app = Flask(__name__)
+CORS(app, resources={r"*": {"origins": "*"}}, supports_credentials=True)
 
-# product 'title' 'content' 'price' 'media'
+product_key = ['title','content', 'price', 'link']
 product = [\
         ["Test Title1", "Test Content1", "0.01", "1.avi"],\
-        ["Test Title2", "Test Content2", "0.02", "2.avi"]\
+        ["Test Title2", "Test Content2", "0.02", "2.avi"],\
+        ["Test Title3", "Test Content3", "0.02", "3.avi"],\
+        ["Test Title4", "Test Content4", "0.02", "4.png"],\
+        ["Test Title5", "Test Content5", "0.02", "5.avi"],\
+        ["Test Title6", "Test Content3", "0.02", "6.avi"],\
+        ["Test Title7", "Test Content4", "0.02", "7.png"],\
+        ["Test Title8", "Test Content5", "0.02", "8.png"],\
+        ["Test Title9", "no image content", "0.00", "8"],\
+        ["Test Title8", "Test Content5", "0.02", "8.png"],\
+        ["Test Title8", "Test Content5", "0.02", "8.png"],\
+        ["Test Title8", "Test Content5", "0.02", "8.png"],\
+        ["Test Title8", "Test Content5", "0.02", "8.png"],\
+        ["Test Title8", "Test Content5", "0.02", "8.png"],\
+        ["Test Title8", "Test Content5", "0.02", "8.png"],\
         ]
 
+def CORS_response(response):
+    new_response = response
+    new_response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+    new_response.headers.add('Access-Control-Allow-Credentials', 'true')
+    return new_response
 
 @app.route("/register_post", methods=['POST'])
 def register_post():
@@ -32,18 +54,35 @@ def register():
 @app.route("/login_post", methods=["POST"])
 def login_post():
     if request.method == "POST":
-        user = request.form["name"]
-        password = request.form["pass"]
-        if user == "admin" and password == "admin":
-            resp = make_response(render_template("index.html"))
-            resp.set_cookie("user", user)
-            return resp
-        elif user == "user" and password == "user":
-            resp = make_response(render_template("index.html"))
-            resp.set_cookie("user", user)
-            return resp
+        try:
+            user = request.json["name"]
+            password = request.json["pass"]
+        except Exception as e:
+            user = request.form["name"]
+            password = request.form["pass"]
+
+        is_admin = user == "admin" and password == "admin"
+        is_user = user == "user" and password == "user"
+
+        if request.headers['Accept'] == 'application/json':
+            resp = jsonify(
+                {
+                    'success':(is_user or is_admin),
+                    'user':(user or ""),
+                    'is_user':is_user,
+                    'is_admin':is_admin
+                }
+            )
         else:
-            return "<p>Login Error</p>"
+            if is_user or is_admin:
+                # if login succeed, redirect to /index
+                resp = make_response(redirect('/index'))
+            else:
+                return "<p>Login Error</p>"
+
+        resp.set_cookie("user", user)
+
+        return resp
 
 
 @app.route("/login")
@@ -55,12 +94,32 @@ def index():
     user = request.cookies.get('user')
     if user == None:
         return redirect(url_for("login"))
+    return render_template("index.html", user=user, product=product)    
 
-    return render_template("index.html", user=user, product=product)
+@app.route("/products")
+def products():
+    user = request.cookies.get('user')
+    print(request.cookies)
+
+    # client에서 cookie 전송이 안되어 임시로 모든 접근 허용
+    # if user == None:
+    #     return abort(403)
+    resp_json = { "products" : []}
+    for raw_product in product:
+        p = {
+            product_key[0] : raw_product[0],
+            product_key[1] : raw_product[1],
+            product_key[2] : raw_product[2],
+            product_key[3] : raw_product[3],
+        }
+        resp_json["products"].append(p)
+    resp = resp_json
+    
+    return resp
 
 @app.route("/")
 def main():
     user = request.cookies.get('user')
     if user == None:
-        return redirect(url_for("login"))
-    return redirect(url_for("index"))
+        return CORS_response(redirect(url_for("login")))
+    return CORS_response(redirect(url_for("index")))
